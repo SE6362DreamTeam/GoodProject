5a5e1a2dc75a892d4fd50c9effdb01f3381c4ec9
mansipatel12 <mansipatel1885@gmail.com>
2024-09-13 19:39:49 -0500

    Added framework for implementing KWIC

---
+++ KWIC.py
@@ -0,0 +1,15 @@
+# FOLLOWING IMPLICIT INVOCATION ARCH: could use boolean flags that are checked periodically
+# Once the flag is true, method begins doing its task (implicitly invoked)
+
+# Class KWIC
+# method readInput:
+
+# method circularshift:
+
+# method alphabetize:
+
+# method storeOutput:
+
+# main method
+    # calls readInput, passes input file to it
+    # calls storeOutput once a flag (lineProcessed is set to true)
