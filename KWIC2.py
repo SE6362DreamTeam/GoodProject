7270ada1fea455750650a61e0ea5fd23afd51b22
LoganCDane <l.c.dane@gmail.com>
2024-09-17 12:44:54 -0500

    Added method defs to implementations
    Added the method definitions to the implementations

--- KWIC2.py
+++ KWIC2.py
@@ -58,18 +58,45 @@ class Output_Interface:
     
 
 class csv_Input(Input_Interface):
+    def __init__(self, lineStorage: LineStorage_Interface) -> None:
+        self.lineStorage = lineStorage
+    def read_input() -> None:
+        pass
     pass
 
 class LineStorage(LineStorage_Interface):
-    pass
+    def __init__(self) -> None:
+        pass
+    def store_input(self) -> None:
+        pass
+    def getLine(line: int):
+        pass
 
 class CircularShift(CircularShift_Interface):
+    def __init__(self, lineStorage: LineStorage_Interface) -> None:
+        pass
+    def retrieve_lines(self) -> None:
+        pass
+    def circular_shift(self) -> None:
+        pass
+    def get_shifts(self) -> list:
+        pass
     pass
 
 class Alphabetize(Alphabetize_Interface):
+    def __init__(self, circularShift: CircularShift_Interface) -> None:
+        pass
+    def alphabetize(self) -> None:
+        pass
+    def get_alphabetized_lines(self) -> list:
+        pass
+    def get_circular_shift_by_index(self, index: i) -> list:
+        pass
     pass
 
 class Output(Output_Interface):
+    def __init__(self, alphabetize: Alphabetize_Interface) -> None:
+        pass
     pass
 
 
